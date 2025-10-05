7b60ec2fd3280a79a2100fe081a005b9e85a0ad5
Ilham <ilham.adios@epitech.eu>
2025-10-05 14:12:01 +0100

    removing the footer for the moment

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
             <main className="flex-1">
               {children}
             </main>
-            <Footer />
+            {/* <Footer /> */}
           </div>
         </ThemeProvider>
       </body>
